bab40b2acfb2611e42bb4b5a8368d5fff9f3e8ed
nabildzr <muhammadnbillz@gmail.com>
2024-06-16 19:14:55 +0700

    fix totally

--- script.js
+++ script.js
@@ -1,117 +1,23 @@
 let url = "https://reqres.in/api/users";
-const content = document.querySelector('#content')
-url = 'data.json'
+const content = document.querySelector("#content");
+url = "data.json";
 const hitAPI = async (url) => {
-    try {
-    const api = await fetch(url)
-    const data = await api.json()
+    const api = await fetch(url);
+    const data = await api.json();
     // console.log(data)
-    return data
-    } catch(error) {
-        console.error("Failed to fetch data:", error)
-    } finally {
-        // hideLoading()
-    }
-    
-} 
+    return data;
+};
 var stylesheetElem = document.querySelector('head link[rel="stylesheet"]');
-// var loader = document.querySelector(".loading");
 
-document.addEventListener('DOMContentLoaded', () => {
-
-    function showLoading() {
-        const loadingIndicator = document.getElementById('loadin'); 
-        if (loadingIndicator) { // Check if the element exists
-            loadingIndicator.style.display = 'block'; 
-        }
-    }
-
-    async function hitAPI() {
-        showLoading(); // Show loading indicator immediately
-
-        fetch('https://kitsu.io/api/edge/anime?filter[categories]=adventure&page[limit]=5')
-            .then(response => response.json())
-            .then(data => {
-                // Process the API data
-                // ...
-data = data.data
-    console.log(data)
-    let teks = ''
-    data.forEach(element => {
-        let attr = element.attributes
-        teks += `
-
-
-               <div style="margin: 18px 13px 10px;">
-                    
-                    <a class="image-awal">
-                    
-                        <img class="image-card" src="${attr.posterImage.small}">
-                    </a>
-                    <div class="box">
-                        <a class="name-9f"> ${attr.titles.en}</a>
-                        <a class="description-1">Check Detail</a><br>
-    
-                        <a class="description-2">Rating: <span class="rating">${attr.averageRating}</span> </a>
-                    </div>
-                </div>
-   
-        `
-
-        //* <a class="friend-text">  ${attr.synopsis}</a>
-        //* <a> ${attr.ageRating}</a>
-        // teks += `<img src="${attr.posterImage.small}"></li>`
-        // teks += '</div>'
-
-        // console.log(element.name)
-    });
-
-    content.innerHTML = teks
-
-                // Hide the loading indicator after data is loaded
-                const loadingIndicator = document.getElementById('loadin');
-                if (loadingIndicator) {
-                    loadingIndicator.style.display = 'none';
-                }
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-            });
-
-            let data = await hitAPI(url);
-    
-    
-    }
-
-    // Trigger the API call (e.g., when a button is clicked)
-    const button = document.getElementById('fetch-data-button');
-    if (button) {
-        button.addEventListener('click', hitAPI);
-    }
-});
-
-// function showLoading() {
-//   loader.style.visibility = "visible"
-//     stylesheetElem.disabled = true;
-// }
-
-// function hideLoading() {
-//     loader.style.visibility = "hidden"
-//     stylesheetElem.disabled = false;
-// }
-
-
-url = "https://kitsu.io/api/edge/anime?filter[categories]=adventure&page[limit]=5"
-document.addEventListener('DOMContentLoaded', async () => {
-    
-    let data = await hitAPI(url);
-    
-    data = data.data
-    console.log(data)
-    let teks = ''
-    data.forEach(element => {
-        let attr = element.attributes
-        teks += `
+url =
+  "https://kitsu.io/api/edge/anime?filter[categories]=adventure&page[limit]=5";
+document.addEventListener("DOMContentLoaded", async () => {
+  let data = await hitAPI(url);
+  data = data.data;
+  let teks = "";
+  data.forEach((element) => {
+    let attr = element.attributes;
+    teks += `
 
 
                <div style="margin: 18px 13px 10px;">
@@ -128,15 +34,15 @@ document.addEventListener('DOMContentLoaded', async () => {
                     </div>
                 </div>
    
-        `
+        `;
 
-        //* <a class="friend-text">  ${attr.synopsis}</a>
-        //* <a> ${attr.ageRating}</a>
-        // teks += `<img src="${attr.posterImage.small}"></li>`
-        // teks += '</div>'
+    //* <a class="friend-text">  ${attr.synopsis}</a>
+    //* <a> ${attr.ageRating}</a>
+    // teks += `<img src="${attr.posterImage.small}"></li>`
+    // teks += '</div>'
 
-        // console.log(element.name)
-    });
+    // console.log(element.name)
+  });
 
-    content.innerHTML = teks
-})
+  content.innerHTML = teks;
+});
