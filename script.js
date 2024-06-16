f91b04b2431660215215c576cd4123c19098f6fa
nabildzr <muhammadnbillz@gmail.com>
2024-06-16 18:10:44 +0700

    Added Loading while Fetching

--- script.js
+++ script.js
@@ -2,45 +2,57 @@ let url = "https://reqres.in/api/users";
 const content = document.querySelector('#content')
 url = 'data.json'
 const hitAPI = async (url) => {
-    showLoading()
+    try {   
     const api = await fetch(url)
     const data = await api.json()
     // console.log(data)
     return data
-    hideLoading()
+    } catch(error) {
+        console.error("Failed to fetch data:", error)
+    } finally {
+        hideLoading()
+    }
+    
 } 
 var stylesheetElem = document.querySelector('head link[rel="stylesheet"]');
+const loader = document.querySelector(".loading");
 
-var loader = document.getElementById("loading")
-
-function showLoading(){
-    loader.style.visibility = "1";
+function showLoading() {
+  loader.style.visibility = "visible"
+    stylesheetElem.disabled = true;
 }
 
-function hideLoading(){
-    loader.style.visibility = "0";
+function hideLoading() {
+    loader.style.visibility = "hidden"
+    stylesheetElem.disabled = false;
 }
+
 
 url = "https://kitsu.io/api/edge/anime?filter[categories]=adventure&page[limit]=5"
 document.addEventListener('DOMContentLoaded', async () => {
+    
     let data = await hitAPI(url);
+    
     data = data.data
     console.log(data)
     let teks = ''
     data.forEach(element => {
         let attr = element.attributes
         teks += `
-   
+
 
                <div style="margin: 18px 13px 10px;">
-                <a><img class="image-card" src="${attr.posterImage.small}"></a>
-                <div class="box">
-                <a class="name-9f"> ${attr.titles.en}</a>
-                <a class="description-1">Check Detail</a><br>
+                    
+                    <a>
+                    
+                        <img class="image-card" src="${attr.posterImage.small}">
+                    </a>
+                    <div class="box">
+                        <a class="name-9f"> ${attr.titles.en}</a>
+                        <a class="description-1">Check Detail</a><br>
     
-                <a class="description-2">Rating: <span class="rating">${attr.averageRating}</span> </a>
-                
-                </div>
+                        <a class="description-2">Rating: <span class="rating">${attr.averageRating}</span> </a>
+                    </div>
                 </div>
    
         `
